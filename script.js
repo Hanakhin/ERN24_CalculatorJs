bb4642062533bc037306864967de9dd2de5414df
hanakhin <hanakhin.nounimassotte@gmail.com>
2024-04-04 16:56:06 +0200

    added colors and classes

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('it works')
+
+
+//ajout d'un element a l'ecran
+
+function appendToDisplay(v){
+    document.getElementById('display').value += v;
+
+}
+
+//Suppression d'un element de l'ecran 
+function clearDisplay(){
+    document.getElementById('display').value = "";
+}
+
+// calcul 
+
+function Calculate(){
+    let expression = document.getElementById('display').value
+    let result = eval(expression)
+    document.getElementById('display').value = result
+}
